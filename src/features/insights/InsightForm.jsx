66a2f5fe977b39d88d6558a89342697926bc32c6
Filippo Dembech <filippo.dembech@gmail.com>
2025-04-08 14:16:02 +0200

    refactor: extract InsightForm

---
+++ src/features/insights/InsightForm.jsx
@@ -0,0 +1,88 @@
+import { Button, IconButton, TextField } from "@mui/material";
+import { Controller, useForm } from "react-hook-form";
+import { GiCancel } from "react-icons/gi";
+
+export default function InsightForm({ className, onClose, onCleanup, onSave }) {
+    const {
+        control,
+        handleSubmit,
+        formState: { errors },
+        reset: resetForm,
+    } = useForm({
+        defaultValues: {
+            title: "",
+            content: "",
+        },
+    });
+
+    function cleanUp() {
+        resetForm?.();
+        onCleanup?.();
+    }
+
+    function onSubmit(data) {
+        onSave?.(data);
+        cleanUp();
+    }
+
+    function closeForm() {
+        onClose?.();
+        cleanUp?.();
+    }
+
+    return (
+        <form
+            className={`relative shadow-cool p-8 flex-col gap-3 rounded-lg mb-3 ${className}`}
+            onSubmit={handleSubmit(onSubmit)}
+        >
+            <span className="absolute top-1 right-1">
+                <IconButton
+                    onClick={closeForm}
+                    size="small"
+                >
+                    <GiCancel />
+                </IconButton>
+            </span>
+            <p>What is the insights about?</p>
+            <Controller
+                name="title"
+                control={control}
+                rules={{
+                    required: "Title is required",
+                }}
+                render={({ field }) => {
+                    return (
+                        <TextField
+                            {...field}
+                            label="Title"
+                            error={errors?.title}
+                            helperText={errors?.title?.message}
+                        />
+                    );
+                }}
+            />
+            <p>What is it?</p>
+            <Controller
+                name="content"
+                control={control}
+                rules={{
+                    required: "Content is required",
+                }}
+                render={({ field }) => (
+                    <TextField
+                        {...field}
+                        label="Content"
+                        error={errors?.content}
+                        helperText={errors?.content?.message}
+                    />
+                )}
+            />
+            <Button
+                variant="contained"
+                type="submit"
+            >
+                Save
+            </Button>
+        </form>
+    );
+}
--- src/pages/InsightsPage.jsx
+++ src/pages/InsightsPage.jsx
@@ -1,31 +1,15 @@
-import {
-    Button,
-    Divider,
-    IconButton,
-    TextField,
-    Typography,
-} from "@mui/material";
+import { Button, Divider, Typography } from "@mui/material";
 import NavbarLink from "../components/NavbarLink";
-import { useEffect, useState } from "react";
-import { GiCancel } from "react-icons/gi";
+import { useState } from "react";
+import InsightForm from "../features/insights/InsightForm";
 
 export default function InsightsPage() {
     const [isCreating, setIsCreating] = useState(false);
-    const [newTitle, setNewTitle] = useState("");
-    const [newContent, setNewContent] = useState("");
     const [insights, setInsights] = useState([]);
-    const [errors, setErrors] = useState({});
-
-    function resetForm() {
-        setNewTitle("");
-        setNewContent("");
-        setErrors({});
-    }
     
-    useEffect(() => {
-        if (!errors) return
-        console.log("hei")
-    }, [errors])
+    function addInsight(insight) {
+        setInsights(curr => [ ...curr, insight])
+    }
 
     return (
         <div className="p-8 flex flex-col gap-8">
@@ -58,81 +42,31 @@ export default function InsightsPage() {
             <div>
                 <Typography variant="h3">Your Insights</Typography>
                 <div className="mt-4">
-                    <div className="mb-3 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
-                        {insights.map((insight, i) => (
-                            <div
-                                key={i}
-                                className="shadow-cool rounded-xl p-5 flex flex-col gap-2"
-                            >
-                                <Typography
-                                    variant="h5"
-                                    fontWeight={500}
-                                >
-                                    {insight.title}
-                                </Typography>
-                                <Divider />
-                                <p>{insight.content}</p>
-                            </div>
-                        ))}
-                    </div>
-                    <div
-                        style={{ display: isCreating ? "flex" : "none" }}
-                        className="relative shadow-cool p-8 flex-col gap-3 rounded-lg mb-3"
-                    >
-                        <span className="absolute top-1 right-1">
-                            <IconButton
-                                onClick={() => setIsCreating(false)}
-                                size="small"
-                            >
-                                <GiCancel />
-                            </IconButton>
-                        </span>
-                        <p>What is the insights about?</p>
-                        <TextField
-                            label="Title"
-                            error={errors?.title}
-                            helperText={errors?.title}
-                            defaultValue={newTitle}
-                            onChange={(e) => setNewTitle(e.target.value)}
-                        />
-                        <p>What is it?</p>
-                        <TextField
-                            label="Content"
-                            error={errors?.content}
-                            helperText={errors?.content}
-                            defaultValue={newContent}
-                            onChange={(e) => setNewContent(e.target.value)}
-                        />
-                        <Button
-                            variant="contained"
-                            onClick={() => {
-                                if (!newTitle)
-                                    setErrors((curr) => ({
-                                        ...curr,
-                                        title: "Title is required.",
-                                    }));
-                                if (!newContent)
-                                    setErrors((curr) => ({
-                                        ...curr,
-                                        content: "Content is required.",
-                                    }));
-                                useState(null)
-                                /*
-                                setInsights((curr) => [
-                                    ...curr,
-                                    { title: newTitle, content: newContent },
-                                ]);
-                                resetForm();
-                                setIsCreating(false);
-                                */
-                            }}
-                        >
-                            Save
-                        </Button>
-                    </div>
                     <Button onClick={() => setIsCreating(true)}>
                         + New Insight
                     </Button>
+                    <InsightForm
+                        className={isCreating ? "flex" : "hidden"}
+                        onSave={(insight) => addInsight(insight)}
+                        onCleanup={() => setIsCreating(false)}
+                    />
+                </div>
+                <div className="mb-3 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
+                    {insights.map((insight, i) => (
+                        <div
+                            key={i}
+                            className="shadow-cool rounded-xl p-5 flex flex-col gap-2"
+                        >
+                            <Typography
+                                variant="h5"
+                                fontWeight={500}
+                            >
+                                {insight.title}
+                            </Typography>
+                            <Divider />
+                            <p>{insight.content}</p>
+                        </div>
+                    ))}
                 </div>
             </div>
         </div>
